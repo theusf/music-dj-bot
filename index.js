7bdf5247f99e73e491c62bc32ed1afe6fdd6811d
Matheus <theus-7@hotmail.com>
2020-06-22 18:06:24 -0300

    added dotenv to deploy to heroku

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const ytdl = require('ytdl-core');
 const Discord = require('discord.js');
 const queue = new Map();
 
+require('dotenv').config();
 
-const {
-    prefix,
-    token,
-} = require('./config.json');
+const token = process.env.TOKEN;
+const prefix = '~';
+
 
 const playlist_urls = [
     'https://www.youtube.com/watch?v=snphzO9UFJM&t=3373s',
     'https://www.youtube.com/watch?v=DtTooerFuiQ&t=13s',
+    'https://www.youtube.com/watch?v=0t9JpOJGwlY&t=436s',
+    'https://www.youtube.com/watch?v=0KvlwMd3C4Y&t=494s',
+    'https://www.youtube.com/watch?v=TsTtqGAxvWk&t=11s',
+    'https://www.youtube.com/watch?v=eKVGPqm9ZcY',
+    'https://www.youtube.com/watch?v=ND9W43gqxas&t=513s',
+    'https://www.youtube.com/watch?v=dIk4rhzs__k',
+    'https://www.youtube.com/watch?v=J87x1yMCbCM',
+    'https://www.youtube.com/watch?v=M-5zMRLxXcw'
 ]
-
-
 
 
 const client = new Discord.Client();
@@ -33,7 +39,7 @@ client.once('disconnect', () => {
 });
 
 client.on('message', async message => {
-  
+
     if (message.author.bot) return; // if message from bots
 
     if (!message.content.startsWith(prefix)) return; // if dont start with prefix
@@ -43,38 +49,28 @@ client.on('message', async message => {
     if (message.content.startsWith(`${prefix}play`)) {
         execute(message, serverQueue);
         return;
-       } 
-       else if (message.content.startsWith(`${prefix}skip`)) {
+    }
+    else if (message.content.startsWith(`${prefix}skip`) || message.content.startsWith(`${prefix}stop`)) {
         skip(message, serverQueue);
         return;
-       } 
-       else if (message.content.startsWith(`${prefix}stop`)) {
-        stop(message, serverQueue);
-        return;
-       } 
-       else {
-        message.channel.send('You need to enter a valid command!')
-       }
-       
-
-    });
+    }
+    else {
+        message.channel.send(`
+        You need to enter a valid command! 
+        ~play - to play a random lofi playlist or chnage the playlist
+        ~stop - stop playing
+        ~skip - stop playing
+        `)
+    }
 
 
+});
 
 
 async function execute(message, serverQueue) {
-    
-    const voiceChannel = message.member.voice.channel ;
 
-    const url = playlist_urls[Math.floor(Math.random() * playlist_urls.length)];
+    voiceChannel = message.member.voice.channel;
 
-    const songInfo = await ytdl.getInfo(url);
-
-    const song = {
-		title: songInfo.title,
-		url: songInfo.video_url,
-    };
-    
     const queueContruct = {
         textChannel: message.channel,
         voiceChannel: voiceChannel,
@@ -86,9 +82,6 @@ async function execute(message, serverQueue) {
 
     queue.set(message.guild.id, queueContruct);
 
-    queueContruct.songs.push(song);
-    
-
     try {
         var connection = await voiceChannel.join();
         queueContruct.connection = connection;
@@ -99,46 +92,62 @@ async function execute(message, serverQueue) {
         return message.channel.send(err);
     }
 
-    play(message.guild, queueContruct.songs[0]);
-    
+    const song = await play(message.guild);
+
     return message.channel.send(`${song.title} is now playing!`);
 
-   }
+}
 
 
 function skip(message, serverQueue) {
-    if (!message.member.voiceChannel) 
-        return message.channel.send('You have to be in a voice channel to stop the music!');
-    if (!serverQueue) 
-        return message.channel.send('There is no song that I could skip!');
+    voiceChannel = message.member.voice.channel;
 
-        const voiceChannel = message.member.voice.channel ;
-      voiceChannel.leave();
+    if (!voiceChannel)
+        return message.channel.send('You have to be in a voice channel to stop the music!');
+    if (!serverQueue)
+        return message.channel.send(`Unfortunately, I'm not playing any lo-fi right now`);
+
+    serverQueue.voiceChannel.leave();
+    queue.delete(message.guild.id);
+
 
 }
 
-function play(guild, song) {
-	const serverQueue = queue.get(guild.id);
+async function play(guild) {
+    const serverQueue = queue.get(guild.id);
 
-	if (!song) {
-		serverQueue.voiceChannel.leave();
-		queue.delete(guild.id);
-		return;
+    const url = playlist_urls[Math.floor(Math.random() * playlist_urls.length)];
+
+    const songInfo = await ytdl.getInfo(url);
+
+    const song = {
+        title: songInfo.title,
+        url: songInfo.video_url,
+    };
+
+    if (!song) {
+        serverQueue.voiceChannel.leave();
+        queue.delete(guild.id);
+        return;
     }
 
+    const stream = ytdl(song.url, { filter: 'audioonly' });
 
-    const stream = ytdl(song.url,  { filter: 'audioonly' });
-
-	const dispatcher = serverQueue.connection.play(stream)
-		.on('end', () => {
-			console.log('Music ended!');
-			serverQueue.songs.shift();
-			play(guild, serverQueue.songs[0]);
-		})
-		.on('error', error => {
-			console.error(error);
+    const dispatcher = serverQueue.connection.play(stream)
+        .on('end', () => {
+            console.log('Music ended!');
+            serverQueue.songs.shift();
+            play(guild);
+        })
+        .on('error', error => {
+            console.error(error);
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
         });
-        
-	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+    return song
 }
+
 
